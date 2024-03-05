d4321af20c881f87bd7ee6538235638f65a3aab1
pray3m <prem.gtm9@gmail.com>
2024-03-06 01:14:09 +0545

    part 2 : Completed ☑️

--- part2/dataforcountries/src/services/weather.js
+++ part2/dataforcountries/src/services/weather.js
@@ -2,8 +2,6 @@ import axios from "axios";
 
 // https://api.openweathermap.org/data/2.5/weather?q=London&appid={API key}
 
-// VITE_OPEN_WEATHER_API_KEY=f907864099832b6d191703531d74165d
-
 const api_key = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
 
 const baseUrl = "https://api.openweathermap.org/data/2.5/weather";
