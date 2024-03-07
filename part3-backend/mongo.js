73db8e2e28cdaf412bef752d3c5b50ba61b04695
pray3m <prem.gtm9@gmail.com>
2024-03-08 01:55:16 +0545

    Exercise 3.12: Command-line database mongodb

---
+++ part3-backend/mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+
+const url = `mongodb+srv://prem:${password}@cluster0.xunejtq.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (name && number) {
+  const person = new Person({
+    name: name,
+    number: number,
+  });
+
+  person.save().then((result) => {
+    console.log(`added ${person.name} ${person.number} to phonebook`);
+  });
+}
+
+Person.find({}).then((result) => {
+  console.log("Phonebook:");
+  result.forEach((person) => {
+    console.log(`${person.name} ${person.number}`);
+  });
+  mongoose.connection.close();
+});
