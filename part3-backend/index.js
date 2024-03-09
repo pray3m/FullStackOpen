042017c1d6bc84210f5cfff85b4201d5f41fc063
pray3m <prem.gtm9@gmail.com>
2024-03-10 00:54:42 +0545

    Exercise 3.19 : Validation

--- part3-backend/index.js
+++ part3-backend/index.js
@@ -60,7 +60,7 @@ app.delete("/api/persons/:id", (req, res, next) => {
 app.post("/api/persons", (req, res, next) => {
   const body = req.body;
 
-  if (!body.name || !body.number) {
+  if (body.name === undefined || body.number === undefined) {
     return res.status(400).json({
       error: "name or number is missing",
     });
@@ -80,14 +80,13 @@ app.post("/api/persons", (req, res, next) => {
 });
 
 app.put("/api/persons/:id", (req, res, next) => {
-  const body = req.body;
+  const { name, number } = req.body;
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  };
-
-  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    req.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: "query" }
+  )
     .then((updatedPerson) => {
       res.json(updatedPerson);
     })
@@ -105,6 +104,8 @@ const errorHandler = (error, req, res, next) => {
 
   if (error.name === "CastError") {
     return res.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return res.status(400).json({ error: error.message });
   }
 
   next(error);
--- part3-backend/models/person.js
+++ part3-backend/models/person.js
@@ -13,7 +13,11 @@ mongoose
   .catch((error) => console.log("Error connecting to MongoDB:", error.message));
 
 const personSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
   number: String,
 });
 
