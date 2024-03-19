a11bd198a5b2ae09212b9f3ab5ed5713c0ecaf87
pray3m <prem.gtm9@gmail.com>
2024-03-19 15:21:05 +0545

    Exercise 4.21 | Blog can be deleted only by the authorized user

--- part4/controllers/blog.js
+++ part4/controllers/blog.js
@@ -3,14 +3,6 @@ const jwt = require("jsonwebtoken");
 const Blog = require("../models/blog");
 const User = require("../models/user");
 
-const getTokenFrom = (req) => {
-  const authorization = req.get("authorization");
-  if (authorization && authorization.startsWith("Bearer ")) {
-    return authorization.replace("Bearer ", "");
-  }
-  return null;
-};
-
 blogsRouter.get("/", async (req, res) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   res.json(blogs);
@@ -30,7 +22,7 @@ blogsRouter.get("/:id", async (req, res, next) => {
 });
 
 blogsRouter.post("/", async (req, res, next) => {
-  const { title, author, url, likes, userId } = req.body;
+  const { title, author, url, likes } = req.body;
 
   if (!title || !url) {
     return res.status(400).json({ error: "Title and url are required" });
@@ -42,8 +34,6 @@ blogsRouter.post("/", async (req, res, next) => {
       return res.status(401).json({ error: "token invalid" });
     }
 
-    console.log(decodedToken);
-
     const user = await User.findById(decodedToken.id);
 
     const blog = new Blog({
@@ -51,7 +41,7 @@ blogsRouter.post("/", async (req, res, next) => {
       author,
       url,
       likes: likes || 0,
-      user: userId,
+      user: decodedToken.id,
     });
 
     const savedBlog = await blog.save();
@@ -66,8 +56,25 @@ blogsRouter.post("/", async (req, res, next) => {
 
 blogsRouter.delete("/:id", async (req, res, next) => {
   try {
-    const result = await Blog.findByIdAndDelete(req.params.id);
-    if (result) res.status(204).end();
+    const decodedToken = jwt.verify(req.token, process.env.SECRET);
+    if (!decodedToken) {
+      return res.status(401).json({ error: "token invalid" });
+    }
+
+    const blogToDelete = await Blog.findById(req.params.id);
+    if (!blogToDelete) {
+      return res.status(404).json({ error: "Blog not found" });
+    }
+
+    if (blogToDelete.user.toString() !== decodedToken.id)
+      return res.status(401).json({ error: "Unauthorized deletion" });
+
+    const result = await Blog.deleteOne(blogToDelete);
+    if (result.deletedCount === 1) {
+      res.status(204).end();
+    } else {
+      res.status(400).json({ error: "Blog deletion failed" });
+    }
   } catch (exception) {
     next(exception);
   }
