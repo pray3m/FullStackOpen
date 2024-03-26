19a84284f2a11782f5a6e925da5347f60344a989
pray3m <prem.gtm9@gmail.com>
2024-03-26 08:58:31 +0545

    Exercise 5.7 | view or hide blog details

--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} - {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 20,
+    border: "solid",
+    borderWidth: 1,
+    marginTop: 15,
+    marginBottom: 5,
+  };
+
+  const [visible, setVisible] = useState(false);
+
+  const showWhenVisible = { display: visible ? "" : "none" };
+
+  return (
+    <div style={blogStyle}>
+      <h3>
+        {blog.title}{" "}
+        <button onClick={() => setVisible(!visible)}>
+          {visible ? "hide" : "view"}
+        </button>
+      </h3>
+      <div style={showWhenVisible}>
+        <a href={blog.url}>{blog.url}</a>
+        <p>
+          Likes : {blog.likes} <button>👍</button>
+        </p>
+        <p> - {blog.author}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
