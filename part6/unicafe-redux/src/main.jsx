fc8f931a14e808fbcc9a52ea587a2d0395c303b8
pray3m <prem.gtm9@gmail.com>
2024-04-05 15:52:20 +0545

    Exercise 6.2: Unicafe Revisited, step2

--- part6/unicafe-redux/src/main.jsx
+++ part6/unicafe-redux/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { createStore } from "redux";
+import { legacy_createStore as createStore } from "redux";
 import reducer from "./reducer";
 
 const store = createStore(reducer);
