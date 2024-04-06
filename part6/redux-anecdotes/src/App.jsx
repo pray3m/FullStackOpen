45e3571d425a1c196c7c517128b935d5d9d8f74c
pray3m <prem.gtm9@gmail.com>
2024-04-06 19:15:28 +0545

    Exercise 6.8 | Separate the AnecdoteList component

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,29 +1,11 @@
-import { useDispatch, useSelector } from "react-redux";
 import AnecdoteForm from "./components/AnecdoteForm";
-import { voteFor } from "./reducers/anecdoteReducer";
+import AnecdoteList from "./components/AnecdoteList";
 
 const App = () => {
-  const dispatch = useDispatch();
-  const anecdotes = useSelector((state) => state);
-
-  const vote = (id) => {
-    dispatch(voteFor(id));
-  };
-
-  const sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes);
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {sortedAnecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      ))}
+      <AnecdoteList />
       <AnecdoteForm />
     </div>
   );
---
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,28 @@
+import { useDispatch, useSelector } from "react-redux";
+import { voteFor } from "../reducers/anecdoteReducer";
+
+const AnecdoteList = () => {
+  const dispatch = useDispatch();
+  const anecdotes = useSelector((state) => state);
+
+  const vote = (id) => {
+    dispatch(voteFor(id));
+  };
+  const sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes);
+
+  return (
+    <div>
+      {sortedAnecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AnecdoteList;
